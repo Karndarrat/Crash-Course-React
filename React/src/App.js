7d2c04a8b64fadfdbd827aaa11c1bbf2f4a1bb5d
karndarrat-n <karndarrat.n@fwd.com>
2024-02-26 14:27:37 +0700

    add function like

--- React/src/App.js
+++ React/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Navbar from './Navbar';
 import Input from './Input';
 import Post from './Post';
+import { func } from 'prop-types';
 
 let id = 1;
 
@@ -11,13 +12,21 @@ function App() {
   const [posts, setPosts] = useState([]);
 
   function addPost(newPost) {
-    setPosts([{ id, title: newPost }, ...posts]);
+    setPosts([{ id, title: newPost, like: false }, ...posts]);
     id += 1;
   }
 
   function removePost(id) {
     const updatedPosts = posts.filter((post) => post.id !== id);
     setPosts(updatedPosts);
+  }
+
+  function likePost(id) {
+    setPosts((prevPosts) =>
+      prevPosts.map((post) =>
+        post.id === id ? { ...post, like: !post.like } : post
+      )
+    );
   }
 
   return (
@@ -27,9 +36,9 @@ function App() {
       {posts.map((post) => (
         <Post
           key={post.id}
-          id={post.id}
-          title={post.title}
+          post={post}
           removePost={removePost}
+          likePost={likePost}
         />
       ))}
     </>
--- React/src/Post.js
+++ React/src/Post.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Post({ id, title, removePost }) {
+function Post({ post, removePost, likePost }) {
+  const {id,title,like} = post
+
   return (
     <div className="Post">
       <button className="Post__delete" onClick={() => removePost(id)}>
@@ -11,15 +13,23 @@ function Post({ id, title, removePost }) {
       <img
         className="Post__image"
         src={`https://source.unsplash.com/random?sig=${id}`}
+        alt={`Post ${id}`}
       />
+      <button className='Post__emotion' onClick={() => likePost(id)}>
+      {like ? 'Unlike' : 'Like'}
+      </button>
     </div>
   );
 }
 
 Post.propTypes = {
-  id: PropTypes.number.isRequired,
-  title: PropTypes.string.isRequired,
-  removePost: PropTypes.func.isRequired
+  post: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    like: PropTypes.bool.isRequired,
+  }).isRequired,
+  removePost: PropTypes.func.isRequired,
+  likePost: PropTypes.func.isRequired
 };
 
 export default Post;
